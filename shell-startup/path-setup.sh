567443e70622e74dc604b725df325b187d0c3c56
Kenneth Moreland <kmorel@sandia.gov>
2011-01-24 12:13:20 -0700

    Detect system path rather than try to maintain it.
    Splitting the zsh config into two files (.zshenv and .zshrc) caused
    a problem where blackrose was setting the path after when .zshenv was
    sourced (when the system path was captured) but before .zshrc was
    sourced (when directories were added to the path using the previously
    captured path).  Now the path setup detects the system path
    independently each time it is called by stripping of the before/after
    paths.  It also checks to make sure the same before/after path is not
    added multiple times.

--- shell-startup/path-setup.sh
+++ shell-startup/path-setup.sh
@@ -3,16 +3,14 @@
 # Gets ready to add custom things to the path and adds the paths that I
 # consistently use.  This script can be sourced by sh and any variant.
 
-if echo ${PATH} | fgrep -v "${HOME}/local/bin" >& /dev/null
-then
-    add_to_path=1
-else
-    add_to_path=0
-fi
+# before_system_path=
+# system_path=${PATH}
+# after_system_path=
 
-before_system_path=
-system_path=${PATH}
-after_system_path=
+# Used internally
+detect_system_path() {
+    system_path=`echo ${PATH} | sed -e "s|${before_system_path:-/not/a/path}||g" -e "s|${after_system_path}:\.||g"`
+}
 
 # Used internally
 update_path() {
@@ -21,12 +19,13 @@ update_path() {
 
 # Add one or more directories to PATH before the system paths.
 add_before_system_path() {
-    if [ "${add_to_path}" != "1" ] ; then
-	return
-    fi
+    detect_system_path
     for dir ; do
 	if [ -d $dir ] ; then
-	    before_system_path=${before_system_path}${dir}:
+	    if echo ${before_system_path} | fgrep -v ${dir}: > /dev/null
+	    then
+		before_system_path=${before_system_path}${dir}:
+	    fi
 	fi
     done
     update_path
@@ -34,12 +33,13 @@ add_before_system_path() {
 
 # Add one or more directories to PATH after the system paths.
 add_after_system_path() {
-    if [ "${add_to_path}" != "1" ] ; then
-	return
-    fi
+    detect_system_path
     for dir ; do
 	if [ -d $dir ] ; then
-	    after_system_path=${after_system_path}:${dir}
+	    if echo ${after_system_path} | fgrep -v :${dir} > /dev/null
+	    then
+		after_system_path=${after_system_path}:${dir}
+	    fi
 	fi
     done
     update_path
