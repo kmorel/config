dd3f230e9e0d4af797cde138c928d11f06b11ee3
Kenneth Moreland <kmorel@sandia.gov>
2012-01-21 16:44:00 -0700

    Update install.bash to work with paths with spaces in them.
    Common on cygwin.

--- install.bash
+++ install.bash
@@ -3,24 +3,24 @@
 # Simple script to install this configuration in the current home directory.
 
 # Relative path to configure dir (the directory this script is located).
-config_dir=$(dirname $BASH_SOURCE)
+config_dir=$(dirname "$BASH_SOURCE")
 
 # Absolute path to configure dir
-export KMOREL_CONFIG_DIR=$(${config_dir}/bin/fullpath $config_dir)
+export KMOREL_CONFIG_DIR=$("${config_dir}/bin/fullpath" "$config_dir")
 
 setup_login_file() {
     login_file=$1
     config_file=$2
-    source_line=". $config_file"
+    source_line=". '$config_file'"
 
-    if [ -f $login_file ] ; then
-	if fgrep "$source_line" $login_file > /dev/null ; then
+    if [ -f "$login_file" ] ; then
+	if fgrep "$source_line" "$login_file" > /dev/null ; then
 	    :
 	else
-	    mv $login_file $login_file.old
-	    echo $source_line > $login_file
-	    echo >> $login_file
-	    cat $login_file.old >> $login_file
+	    mv "$login_file" "$login_file.old"
+	    echo $source_line > "$login_file"
+	    echo >> "$login_file"
+	    cat "$login_file.old" >> "$login_file"
 
 	    echo "#######################################################"
 	    echo "Added the following to $login_file:"
@@ -33,7 +33,7 @@ setup_login_file() {
 	    echo
 	fi
     else
-	echo $source_line > $login_file
+	echo $source_line > "$login_file"
 
 	echo "#######################################################"
 	echo "Created $login_file"
@@ -45,13 +45,13 @@ setup_symbolic_link() {
     target=$1
     source=$2
 
-    if [ -f $target ] ; then
-	if [ -L $target -a "$(readlink $target)" = $source ] ; then
+    if [ -f "$target" ] ; then
+	if [ -L "$target" -a "$(readlink '$target')" = "$source" ] ; then
 	    # Link is already correct.
 	    :
 	else
-	    mv $target $target.old
-	    ln -s $source $target
+	    mv "$target" "$target.old"
+	    ln -s "$source" "$target"
 
 	    echo "#######################################################"
 	    echo "Moved $target to $target.old"
@@ -59,7 +59,7 @@ setup_symbolic_link() {
 	    echo
 	fi
     else
-	ln -s $source $target
+	ln -s "$source" "$target"
 
 	echo "#######################################################"
 	echo "Created $target"
@@ -68,9 +68,9 @@ setup_symbolic_link() {
     fi
 }
 
-setup_login_file ${HOME}/.bashrc ${KMOREL_CONFIG_DIR}/bashrc.bash
-setup_login_file ${HOME}/.zshrc ${KMOREL_CONFIG_DIR}/zshrc.zsh
-setup_login_file ${HOME}/.zshenv ${KMOREL_CONFIG_DIR}/zshenv.zsh
+setup_login_file "${HOME}/.bashrc" "${KMOREL_CONFIG_DIR}/bashrc.bash"
+setup_login_file "${HOME}/.zshrc" "${KMOREL_CONFIG_DIR}/zshrc.zsh"
+setup_login_file "${HOME}/.zshenv" "${KMOREL_CONFIG_DIR}/zshenv.zsh"
 
-setup_symbolic_link ${HOME}/.gitconfig ${KMOREL_CONFIG_DIR}/gitconfig
-setup_symbolic_link ${HOME}/.gitignore ${KMOREL_CONFIG_DIR}/gitignore
+setup_symbolic_link "${HOME}/.gitconfig" "${KMOREL_CONFIG_DIR}/gitconfig"
+setup_symbolic_link "${HOME}/.gitignore" "${KMOREL_CONFIG_DIR}/gitignore"
