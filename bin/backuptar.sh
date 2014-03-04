57eca6a6404ecc6ab68e8c6953e2bebb45af18ae
Kenneth Moreland <kmorel@sandia.gov>
2014-03-04 13:39:51 -0700

    Change backup script to split large backup files.
    Prevents any single backup file from being too large to fit on DVD.
    
    This change will cause problems if the collective files in a directory
    are too large (that is, they are not split into subdirectories). The
    script will continually try to break down the same directory even though
    it cannot.

--- bin/backuptar.sh
+++ bin/backuptar.sh
@@ -16,25 +16,51 @@ backup_subtarget() {
     cd "$target"
     if [ -d "$subtarget" ] ; then
         echo "Packaging files in $target/$subtarget"
-	subtarget_name=`echo $subtarget | tr / _`
+	subtarget_name=`echo $subtarget | tr '/ ' __`
 	if [ "$subtarget" = "." ] ; then
-	    subtarget_name=`echo $target | tr / _`
+	    subtarget_name=`echo $target | tr '/ ' __`
 	fi
         backupfile=$backupfile_prefix$subtarget_name.tar.bzip2
 	file_list_file=/tmp/file_list_$subtarget_name
 
 	echo "  finding files, removing exclusions"
 	rm -f $file_list_file
-	eval /usr/bin/find "$target/$subtarget" -type f $exclusion_args $other_find_args | sed "s|^$target/||" > $file_list_file
+	eval /usr/bin/find \'$target/$subtarget\' -type f $exclusion_args $other_find_args | sed "s|^$target/||" > $file_list_file
 
 	if [ -s $file_list_file ] ; then
-	    echo "  archiving to $backupfile"
-	    tar cf $backupfile --bzip2 -T $file_list_file
+	    echo "  estimating file size"
+	    size=`tar c -T $file_list_file | wc -c`
+	    echo "    $size bytes"
+	    if [ $size -lt 4600000000 ] ; then
+		echo "  archiving to $backupfile"
+		tar cf $backupfile --bzip2 -T $file_list_file
+	    else
+		echo "    Archive might not fit on DVD. Split."
+		backup_subtarget "$target" "$subtarget" "-maxdepth 1"
+		for subsubtarget in $subtarget/* ; do
+		    echo "$target" "$subsubtarget"
+		    backup_subtarget "$target" "$subsubtarget"
+		done
+	    fi
 	else
 	    echo "  no files, skipping"
 	fi
 
 	rm -f $file_list_file
+
+	# if [ -f $backupfile ] ; then
+	#     size=`du -sg $backupfile | sed 's/^\([0-9]\)*[^0-9].*/\1/'`
+	#     echo "File fits in $size GB"
+	#     if [ "$size" -gt 4 ] ; then
+	# 	echo "File (possibly) does not fit on DVD. Split."
+	# 	rm -f $backupfile
+	# 	backup_subtarget "$target" "$subtarget" "-maxdepth 1"
+	# 	for subsubtarget in $subtarget/* ; do
+	# 	    echo "$target" "$subsubtarget"
+	# 	    backup_subtarget "$target" "$subsubtarget"
+	# 	done
+	#     fi
+	# fi
     fi
 }
 
