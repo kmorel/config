f2ecfa02896f2013ab4aaf6218e1d2aed2384528
Kenneth Moreland <morelandkd@ornl.gov>
2022-08-11 08:41:51 -0600

    Work around problem with vim

--- shell-startup/basic-environment.sh
+++ shell-startup/basic-environment.sh
@@ -24,8 +24,16 @@ quiet_which() {
 # These are read by some editors (such as emacs) to set the default encoding
 # for unicode text files. You'd think that they would default to UTF-8, but
 # not always.
-LANG=en_US.UTF-8
-LANGUAGE=en_US.UTF-8
-LC_ALL=en_US.UTF-8
+export LANG=en_US.UTF-8
+export LANGUAGE=en_US.UTF-8
+export LC_ALL=en_US.UTF-8
+
+# I am currently having a problem with vim refreshing its screen unless
+# you set LANG to C, which overrides any localization to use standard C
+# syntax (e.g. letters are a-z, numbers us . for decimal). This is
+# pretty close to US encoding anyway.
+export LANG=C
+export LANGUAGE=C
+export LC_ALL=C
 
 export LATEX_DEFAULT_BUILD=dvi
